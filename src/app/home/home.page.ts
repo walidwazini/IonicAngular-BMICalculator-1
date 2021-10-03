3bf9ae64f6c299d75132e6d576e81657ba241c48
Walid Wazini <wazini25@gmail.com>
2021-10-03 09:49:32 +0800

    sir punya logics

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {
+  ToastController,
+  ActionSheetController,
+  AlertController,
+} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  weight = 0
+  height = 0
+  bmiV = 0
+
+  weightSir
+  heightSir
+  bmiSir
 
   constructor() {}
+
+  calculateHandler(){
+    this.bmiV = this.weight / ((this.height / 100) ^ 2)
+    console.log(this.bmiV)
+    return this.bmiV
+  }
+
+  calculatePressed(){
+    // toFixed -> 
+    // toPrecision ->
+    this.bmiSir = (this.weightSir / Math.pow((this.heightSir),2)).toFixed(2)
+  }
 
 }
